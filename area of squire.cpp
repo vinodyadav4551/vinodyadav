0bb3ba74ddad6caf10977e2b119a0bc9b55e7ba2
vinodyadav4551 <95622621+vinodyadav4551@users.noreply.github.com>
2021-12-06 17:14:07 +0530

    3rd

---
+++ area of squire.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int area,squire;
+    cout << "enter area in squire\n";  \\user input 
+    cin >> area;                        
+    squire=area*area;                     
+    cout << "\narea of squire\n" << squire;
+    
