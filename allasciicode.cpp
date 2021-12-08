fc89682a739b769cf2232f150ea8deb082d62428
vinodyadav4551 <95622621+vinodyadav4551@users.noreply.github.com>
2021-12-08 15:33:12 +0530

    A-Zascii

---
+++ allasciicode.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+int main()  
+{  
+ int k;   
+ for(int k=33;k<=127;k++)
+ {
+    std::cout << "\nThe ascii value of\n" <<char(k)<<" = " <<k;  
+     }
+
+    return 0;
+}
+   
+  
