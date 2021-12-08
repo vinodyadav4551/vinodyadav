7a6a356fee40d249ba88839714e473e4b5513b78
vinodyadav4551 <95622621+vinodyadav4551@users.noreply.github.com>
2021-12-08 15:35:05 +0530

    onlyonecharacter

---
+++ userinputascii.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char c;
+    cout << "enter a character\n";
+    cin >> c;
+    cout << "the ascii value of the char variable " << c << " = "  << int(c);
+    return 0; 
+    
+}
+
